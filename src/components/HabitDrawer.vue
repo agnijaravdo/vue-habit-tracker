c607d9aa216d5e7750d539b13b84c8e0649b80c4
Agnija Ravdo <41111594+agnijaravdo@users.noreply.github.com>
2024-10-24 02:11:58 +0300

    add emoji picker for habits

--- src/components/HabitDrawer.vue
+++ src/components/HabitDrawer.vue
@@ -1,22 +1,26 @@
 <script setup>
+import { ref } from 'vue'
 import Button from 'primevue/button'
 import Drawer from 'primevue/drawer'
-import InputGroup from 'primevue/inputgroup'
 import InputText from 'primevue/inputtext'
-import { defineProps, defineEmits, ref } from 'vue'
+import EmojiPicker from 'vue3-emoji-picker'
 
 import { addNewHabit, removeHabit, getListOfHabits, getNextHabitId } from '../store/habits'
 
-defineProps({
-  visible: Boolean
-})
+const visible = ref(false)
 const emit = defineEmits(['update:visible'])
-
 const onDrawerClose = () => {
   emit('update:visible', false)
 }
 
-const newHabit = ref(null)
+const newHabit = ref('')
+const showEmojiPicker = ref(false)
+
+const toggleEmojiPicker = () => {
+  console.log('toggleEmojiPicker')
+  showEmojiPicker.value = !showEmojiPicker.value
+  console.log(showEmojiPicker.value)
+}
 
 const addNewHabitToStore = () => {
   if (newHabit.value) {
@@ -27,6 +31,11 @@ const addNewHabitToStore = () => {
     newHabit.value = ''
   }
 }
+
+const onSelectEmoji = (emoji) => {
+  newHabit.value += emoji.i
+  showEmojiPicker.value = false
+}
 </script>
 
 <template>
@@ -36,31 +45,44 @@ const addNewHabitToStore = () => {
         <h1 class="text-2xl font-bold">Tracked Habits</h1>
       </div>
     </template>
-    <TransitionGroup name="list" tag="ul">
-      <li v-for="habit of getListOfHabits()" :key="habit.id">
-        <div class="flex justify-between items-center p-2">
-          <span>{{ habit.name }}</span>
-          <Button
-            icon="pi pi-trash"
-            @click="removeHabit(habit.id)"
-            severity="secondary"
-            class="p-button-text"
-          ></Button>
+    <div class="scrollable-content">
+      <TransitionGroup name="list" tag="ul">
+        <li v-for="habit of getListOfHabits()" :key="habit.id">
+          <div class="flex justify-between items-center p-2">
+            <span>{{ habit.name }}</span>
+            <Button
+              icon="pi pi-trash"
+              @click="removeHabit(habit.id)"
+              severity="secondary"
+              class="p-button-text"
+            ></Button>
+          </div>
+        </li>
+      </TransitionGroup>
+      <div class="mt-4 space-y-3">
+        <div class="flex mb-2 space-x-1" style="width: 105%">
+          <InputText
+            v-model="newHabit"
+            id="new-habit"
+            placeholder="Enter New Habit"
+            class="flex-auto"
+            v-on:keyup.enter="addNewHabitToStore"
+          />
+          <Button icon="pi pi-face-smile" @click="toggleEmojiPicker" severity="secondary" />
         </div>
-      </li>
-      <br v-if="getListOfHabits().length === 0" />
-    </TransitionGroup>
-    <div class="mt-4">
-      <InputGroup class="flex items-center">
-        <InputText
-          v-model="newHabit"
-          id="new-habit"
-          placeholder="Enter New Habit"
-          class="flex-1"
-          v-on:keyup.enter="addNewHabitToStore"
+        <EmojiPicker
+          v-if="showEmojiPicker"
+          @select="onSelectEmoji"
+          style="position: absolute; z-index: 1000; top: 240px; left: 0"
         />
-        <Button icon="pi pi-plus" severity="success" @click="addNewHabitToStore" />
-      </InputGroup>
+        <Button
+          icon="pi pi-plus"
+          label="Add new habit"
+          severity="success"
+          @click="addNewHabitToStore"
+          style="width: 105%"
+        />
+      </div>
     </div>
   </Drawer>
 </template>
@@ -75,6 +97,12 @@ const addNewHabitToStore = () => {
 .list-leave-to {
   opacity: 0;
   transform: translateX(30px);
+}
+
+.scrollable-content {
+  max-height: 100vh;
+  overflow-y: auto;
+  padding-right: 1rem;
 }
 
 ul {
