927d5bf5562f7c3e822b69fa875c6b847a66568c
Agnija Ravdo <41111594+agnijaravdo@users.noreply.github.com>
2024-11-02 18:04:10 +0200

    add main logo icon

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,7 +7,10 @@ defineEmits(['toggle-drawer'])
 <template>
   <div class="flex justify-between items-center p-4 bg-gray-100 border-b">
     <RouterLink to="/">
-      <h1 class="text-xl font-bold cursor-pointer">Habit Hack</h1>
+      <div class="flex items-center space-x-2">
+        <h1 class="text-2xl font-bold cursor-pointer">Habit Hack</h1>
+        <i class="pi pi-calendar-times" style="color: #10b981; font-size: 1.5rem"></i>
+      </div>
     </RouterLink>
     <Button label="Your habits list" icon="pi pi-arrow-left" @click="$emit('toggle-drawer')" />
   </div>
