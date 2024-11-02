d58157db85a8a072ee39e585aff499b65e5458e3
Agnija Ravdo <41111594+agnijaravdo@users.noreply.github.com>
2024-11-02 17:01:44 +0200

    on header click move to home

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,7 +6,9 @@ defineEmits(['toggle-drawer'])
 
 <template>
   <div class="flex justify-between items-center p-4 bg-gray-100 border-b">
-    <h1 class="text-xl font-bold">Habit Hack</h1>
+    <RouterLink to="/">
+      <h1 class="text-xl font-bold cursor-pointer">Habit Hack</h1>
+    </RouterLink>
     <Button label="Your habits list" icon="pi pi-arrow-left" @click="$emit('toggle-drawer')" />
   </div>
 </template>
