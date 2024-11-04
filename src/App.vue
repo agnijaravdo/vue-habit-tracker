14db7d943b5858083ab4e565a519dff4260eac77
Agnija Ravdo <41111594+agnijaravdo@users.noreply.github.com>
2024-11-04 23:46:17 +0200

    add back to today button

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
+import MainFooter from './components/MainFooter.vue'
+import MainHeader from './components/MainHeader.vue'
 import store from './store/store'
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen">
-    <Header @toggle-drawer="store.isDrawerVisible = !store.isDrawerVisible" />
+    <MainHeader @toggle-drawer="store.isDrawerVisible = !store.isDrawerVisible" />
     <main class="flex-1 overflow-y-auto p-10">
       <RouterView />
     </main>
-    <Footer />
+    <MainFooter />
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import DatePicker from 'primevue/datepicker'
+import Button from 'primevue/button'
 import useCalendar from '../store/calendar'
 import store from '../store/store'
 import DateSlider from '../components/DateSlider.vue'
@@ -7,13 +8,14 @@ import HabitDrawer from '../components/HabitDrawer.vue'
 import EmptyState from '../components/EmptyState.vue'
 import DailyHabitsList from '../components/DailyHabitsList.vue'
 
-const { isSelectedDayAFutureDate } = useCalendar()
+const { isSelectedDayAFutureDate, selectDate } = useCalendar()
 </script>
 
 <template>
   <HabitDrawer v-model:visible="store.isDrawerVisible" />
   <div class="w-full max-w-7xl mx-auto px-10">
-    <div class="flex justify-end mb-4">
+    <div class="px-4 flex justify-between mb-4">
+      <Button label="Today" severity="secondary" @click="selectDate(new Date())" />
       <DatePicker v-model="store.dateDisplay" showIcon :showOnFocus="false" />
     </div>
     <DateSlider />
