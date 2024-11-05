9876e89b08855d930497a302cd083662c1d291e2
Agnija Ravdo <41111594+agnijaravdo@users.noreply.github.com>
2024-11-05 02:16:46 +0200

    add global error handling for layout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import { computed } from 'vue'
+import Message from 'primevue/message'
 import MainFooter from './components/MainFooter.vue'
 import MainHeader from './components/MainHeader.vue'
 import store from './store/store'
+import { errorCurrent } from './store/error'
+
+const errorMessage = computed(() => (errorCurrent.value ? errorCurrent.value.message : ''))
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen">
     <MainHeader @toggle-drawer="store.isDrawerVisible = !store.isDrawerVisible" />
     <main class="flex-1 overflow-y-auto p-10">
-      <RouterView />
+      <div v-if="errorCurrent" class="error-message">
+        <Message severity="error">{{ errorMessage }}</Message>
+      </div>
+      <div v-else><RouterView /></div>
     </main>
     <MainFooter />
   </div>
